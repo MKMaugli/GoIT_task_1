a49e5fb1e00cf219d6b44a047b8cd9979132b638
Kosolap Dmytro <mityakosolap@gmail.com>
2023-10-22 12:48:43 +0200

    341

--- Test_1.py
+++ Test_1.py
@@ -1,31 +1,11 @@
-# num1 = int(input("Enter a number: "))
-num1 = input("Enter a number: ")
-num = float(num1)
 
-try:
-    num = float(num1)
-    if num.is_integer():
-        num = int(num)
-    else:
-        print("The number should be an integer.")
-        exit()
-except ValueError:
-    print("Invalid input. Please enter a valid number.")
-    exit()
-
-if num >0:
-    if (num %2) == 0:
-        result = "Positive even number"      #Положительное четное число
-    else:
-        result = "Positive odd number"     #Положительное нечетное число
-elif num < 0:
-    result = "Negative number"
-else:
-    result = "It is zero"
-
-a = num %2
-print (a)
-print (result)
-
-# print (height)
-# print (is_active)
+sum = 0
+while True:
+    num = int(input("Введіть число (0 для виходу): "))
+    if num == 0:
+        break
+    for i in range(num + 1):
+        sum = sum + i
+    print(sum)
+    
+    
---
+++ Test_3.py
@@ -0,0 +1,11 @@
+num = 5
+sum = 0
+sum2= 0
+
+for i in range(num):
+    sum += i
+    i = i+1
+    sum2 += i
+    # print(sum)
+    
+    
