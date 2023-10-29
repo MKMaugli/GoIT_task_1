ecdd2e3fa3e2584613c340cad7be534cba8b4a78
Kosolap Dmytro <mityakosolap@gmail.com>
2023-10-29 21:04:33 +0100

    3.3

--- Test_1.py
+++ Test_1.py
@@ -1,53 +1,11 @@
-result = None
-operand = None
-operator = None
-wait_for_number = True
+base_rate = 40
+price_per_km = 10
+total_trip = 0
 
-while True:
-    if wait_for_number:
-        user_input = input("Enter the number: ")
-        try:
-            operand = float(user_input)
-            if result is None:
-                result = operand
-            wait_for_number = False
-        except ValueError:
-            print(f"'{user_input}' is not a valid number. Try again.")
-    else:
-        user_input = input("Enter the operator (+, -, *, /): ")
-        if user_input in ('+', '-', '*', '/'):
-            if operator == '+':
-                result = result + operand if result is not None else operand
-            elif operator == '-':
-                result = result - operand if result is not None else operand
-            elif operator == '*':
-                result = result * operand if result is not None else operand
-            elif operator == '/':
-                if operand == 0:
-                    print("Division by zero is not allowed. Try again.")
-                    continue
-                result = result / operand if result is not None else operand
 
-            operator = user_input
-            wait_for_number = True
-        elif user_input == '=':
-            if operator is None:
-                print("No operator provided. Try again.")
-            else:
-                if operator == '+':
-                    result = result + operand if result is not None else operand
-                elif operator == '-':
-                    result = result - operand if result is not None else operand
-                elif operator == '*':
-                    result = result * operand if result is not None else operand
-                elif operator == '/':
-                    if operand == 0:
-                        print("Division by zero is not allowed. Try again.")
-                        continue
-                    result = result / operand if result is not None else operand
-                else:
-                    print(f"'{operator}' is not a valid operator. Try again.")
-            print(f"Result: {result}")
-            break
-        else:
-            print(f"'{user_input}' is not '+', '-', '*', '/', or '='. Try again.")
+def calculate_trip_price(distance_km):
+    global total_trip
+    total_trip += 1
+    return (base_rate + price_per_km*distance_km)
+
+print(calculate_trip_price (5))
