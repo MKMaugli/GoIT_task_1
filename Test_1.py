c1608eeb4283f8ccff4b893b5e7914dcbe2aaf25
Kosolap Dmytro <mityakosolap@gmail.com>
2023-11-02 22:20:57 +0100

    3.6

--- Test_1.py
+++ Test_1.py
@@ -1,13 +1,35 @@
-def discount_price(price, discount):
-    float(price)
-    float(discount)         #= float(input("Enter the discount: "))
+def format_string(string, length):
+    string_characters = len(string)
     
-    def apply_discount():
-        nonlocal price
-        price = (price*(1-discount))
+    if string_characters >= length:
+        # print(string)
+        return(string)
+    else:
+        spaces_to_add = max(0, (length - len(string)) // 2)
+        padded_string = " " * spaces_to_add + string
+        # print(padded_string)
+        return(padded_string)
 
-    apply_discount()
-    return price
 
-discount_price(100, 0.1)
-print(discount_price)
+format_string (length=15, string='abaa')
+
+# # Введення рядка
+# user_input = input("Введіть рядок: ")
+# length = 15
+
+# # Обчислення кількості символів у рядку
+# num_characters = len(user_input)
+
+# # Розрахунок кількості пробілів, які треба додати перед рядком
+# spaces_to_add = max(0, (length - len(user_input)) // 2)
+
+# # Додавання пробілів перед рядком
+# padded_string = " " * spaces_to_add + user_input
+
+# # Виведення результату
+# print(f"Кількість символів у введеному рядку: {num_characters}")
+# print(f"Рядок з доданими пробілами: {padded_string}")
+
+# spaces_to_add = max(0, (num_characters - len(length)) // 2)
+
+#get_fullname ("Petro", "Zaliznyak")
--- Test_2.py
+++ Test_2.py
@@ -1,56 +1,8 @@
-result = None
-operand = None
-operator = None
+def get_fullname (first_name, last_name, middle_name=''):
+    if middle_name == '':
+        f'{first_name} {middle_name} {last_name}'
+    else:
+        f'{first_name} {last_name}'
 
-while True:
-    try:
-        operand = float(input("Введите число: "))
-    except ValueError:
-        print("Введено не число. Попробуйте снова.")
-        continue
 
-    while True:
-        operator = input("Введите оператор (+, -, *, /) или '=' для вычисления: ")
-
-        if operator == '=':
-            if result is not None:
-                if operator != '=':
-                    if operator == '+':
-                        result = result + operand
-                    elif operator == '-':
-                        result = result - operand
-                    elif operator == '*':
-                        result = result * operand
-                    elif operator == '/':
-                        if operand != 0:
-                            result = result / operand
-                        else:
-                            print("Деление на ноль невозможно.")
-                    else:
-                        print(f"Результат: {result}")
-                else:
-                    result = operand
-                print(f"Результат: {result}")
-                break
-            else:
-                result = operand
-                break
-        elif operator in "+-*/":
-            if result is not None:
-                if operator == '+':
-                    result = result + operand
-                elif operator == '-':
-                    result = result - operand
-                elif operator == '*':
-                    result = result * operand
-                elif operator == '/':
-                    if operand != 0:
-                        result = result / operand
-                    else:
-                        print("Деление на ноль невозможно.")
-                        break
-            else:
-                result = operand
-            break
-        else:
-            print(f"'{operator}' не является допустимым оператором. Попробуйте снова.")
+print(get_fullname ("Petro", "Zaliznyak"))
