9cc8f6b3d257f1f873b561fd12a47039251d2771
Kosolap Dmytro <mityakosolap@gmail.com>
2023-11-02 22:44:19 +0100

    3.7

--- Test_1.py
+++ Test_1.py
@@ -1,35 +1,14 @@
-def format_string(string, length):
-    string_characters = len(string)
-    
-    if string_characters >= length:
-        # print(string)
-        return(string)
-    else:
-        spaces_to_add = max(0, (length - len(string)) // 2)
-        padded_string = " " * spaces_to_add + string
-        # print(padded_string)
-        return(padded_string)
+def first(size, *args):
+    return size + len(args)
 
+# Пример вызова функции:
+result = first(5, "first", "second", "third")
+print(result)  # Выведет число: 8 (5 + 3)
 
-format_string (length=15, string='abaa')
+def second(size, **args):
+    return size + len(args)
 
-# # Введення рядка
-# user_input = input("Введіть рядок: ")
-# length = 15
+second(3, comment_one="first", comment_two="second", comment_third="third")
 
-# # Обчислення кількості символів у рядку
-# num_characters = len(user_input)
-
-# # Розрахунок кількості пробілів, які треба додати перед рядком
-# spaces_to_add = max(0, (length - len(user_input)) // 2)
-
-# # Додавання пробілів перед рядком
-# padded_string = " " * spaces_to_add + user_input
-
-# # Виведення результату
-# print(f"Кількість символів у введеному рядку: {num_characters}")
-# print(f"Рядок з доданими пробілами: {padded_string}")
-
-# spaces_to_add = max(0, (num_characters - len(length)) // 2)
-
-#get_fullname ("Petro", "Zaliznyak")
+result = second(3, comment_one="first", comment_two="second", comment_third="third")
+print(result)
