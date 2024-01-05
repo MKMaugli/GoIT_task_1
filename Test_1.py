1d13c79146bfa95f26af28424f6605bef8dfbee3
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-05 16:03:57 +0100

    4.7

--- Test_1.py
+++ Test_1.py
@@ -1,17 +1,44 @@
-def split_list(scores):
-    if not scores:
-        # Якщо список порожній, повертаємо два порожні списки.
-        return [], []
+points = {
+    (0, 1): 2,
+    (0, 2): 3.8,
+    (0, 3): 2.7,
+    (1, 2): 2.5,
+    (1, 3): 4.1,
+    (2, 3): 3.9,
+}
 
-    average_score = sum(scores) / len(scores)
-    lower_half = [score for score in scores if score <= average_score]
-    upper_half = [score for score in scores if score > average_score]
 
-    return lower_half, upper_half
+def calculate_distance(coordinates):
+    if not coordinates or len(coordinates) == 1:
+        # Для порожнього списку або списку з однією координатою повертаємо 0.
+        return 0
+
+    points = {(0, 1): 2, (0, 2): 3.8, (0, 3): 2.7, (1, 2): 2.5, (1, 3): 4.1, (2, 3): 3.9}
+
+    total_distance = 0
+    for i in range(len(coordinates) - 1):
+        point1, point2 = min(coordinates[i], coordinates[i + 1]), max(coordinates[i], coordinates[i + 1])
+        total_distance += points.get((point1, point2), 0)
+
+    return total_distance
 
 # Приклад використання:
-student_scores = [75, 88, 92, 60, 78, 85, 95]
-lower, upper = split_list(student_scores)
+quad_coordinates = [0, 1, 3, 2, 0]
 
-print("Нижня половина:", lower)
-print("Верхня половина:", upper)
+result_distance = calculate_distance(quad_coordinates)
+print(f"Загальна відстань пролетіла дрон: {result_distance}")
+
+    
+    
+        
+    
+    
+    
+        
+        
+        
+            
+        
+            
+        
+    
