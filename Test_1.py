45caecabf71475e6219f6876eb09c3cf4e09becc
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-05 20:13:02 +0100

    5.9

--- Test_1.py
+++ Test_1.py
@@ -1,17 +1,20 @@
-grades = {"A": 5, "B": 5, "C": 4, "D": 3, "E": 3, "FX": 2, "F": 1}
-
-def formatted_grades(students):
+def formatted_numbers():
     result = []
 
-    for i, (name, grade) in enumerate(students.items(), start=1):
-        points = grades.get(grade, 0)
-        formatted_line = "{:>4}|{:<10}|{:^5}|{:^5}".format(i, name, grade, points)
+    header = "| decimal  |   hex    |  binary  |"
+    format_str = "|{:<10}|{:^10}|{:>10}|"
+
+    result.append(header)
+
+    for num in range(16):
+        decimal = num
+        hex_num = hex(num)[2:]
+        binary = bin(num)[2:]
+        formatted_line = format_str.format(decimal, hex_num, binary)
         result.append(formatted_line)
 
     return result
 
-# Приклад використання
-students = {"Nick": "A", "Olga": "B", "Mike": "FX", "Anna": "C"}
-
-for el in formatted_grades(students):
+# Виведемо результат
+for el in formatted_numbers():
     print(el)
