e66bf3962316880e11f053cff93a8dc8527a3283
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-05 18:21:47 +0100

    4.13

--- Test_1.py
+++ Test_1.py
@@ -1,54 +1,41 @@
-from random import randint
+from pathlib import Path  # Додаємо імпорт Path
+
+print("Поточна директорія:", Path.cwd())
+
+def parse_folder(path):
+    files = []
+    folders = []
+    
+    for item in path.iterdir():
+        if item.is_file():
+            files.append(item.name)
+        elif item.is_dir():
+            folders.append(item.name)
+            
+    return files, folders
 
 
-def get_random_password():
-    result = ""
-    count = 0
-    while count < 8:
-        random_symbol = chr(randint(40, 126))
-        result = result + random_symbol
-        count = count + 1
-    return result
+# Замість цього
+# result_files, result_folders = parse_folder
 
+# Викликаємо функцію без витягування результату в змінні
+# parse_folder()
 
-def is_valid_password(password):
-    if len(password) != 8:
-        return False
+# Викликаємо функцію та отримуємо результат
+# result_files, result_folders = parse_folder()
 
-    has_upper = False
-    has_lower = False
-    has_num = False
+result_files, result_folders = parse_folder(Path('C:\_testData'))
 
-    for ch in password:
-        if ch.isupper():
-            has_upper = True
-        elif ch.islower():
-            has_lower = True
-        elif ch.isdigit():
-            has_num = True
+if result_files:
+    print("Знайдені файли:")
+    for file_path in result_files:
+        print(file_path)
 
-    return has_upper and has_lower and has_num
+if result_folders:
+    print("Знайдені теки:")
+    for folder_path in result_folders:
+        print(folder_path)
 
-def get_password():
-    max_attempts = 100
-    current_attempt = 0
+if not result_files and not result_folders:
+    print(f"В текі не знайдено файлів або тек.")
 
-    while current_attempt < max_attempts:
-        password_candidate = get_random_password()
-
-        if is_valid_password(password_candidate):
-            return password_candidate
-
-        current_attempt += 1
-
-    # Якщо не вдається згенерувати надійний пароль за обмежену кількість спроб,
-    # можна повернути None або викинути виняток, залежно від вашого бажання.
-    return None
-
-# Приклад використання:
-generated_password = get_password()
-
-if generated_password:
-    print(f"Згенерований надійний пароль: {generated_password}")
-else:
-    print("Не вдалося згенерувати надійний пароль за обмежену кількість спроб.")
--- Test_2.py
+++ Test_2.py
@@ -1,16 +1,37 @@
-def game(list_of_lists, power):
-    for sublist in list_of_lists:
-        for value in sublist:
-            if value <= power:
-                power += value
-            else:
-                break
+from pathlib import Path
 
-    return power
+def parse_folder(path):
+    files = []
+    folders = []
+    
+    directory_path = Path(path)
+
+    if not directory_path.is_dir():
+        print(f"Шлях {path} не вказує на теку або такий шлях не існує.")
+        return files, folders
+
+    for item in directory_path.iterdir():
+        if item.is_file():
+            files.append(item)
+        elif item.is_dir():
+            folders.append(item)
+
+    return files, folders
 
 # Приклад використання:
-example_list = [[1, 1, 5, 10], [10, 2], [1, 1, 1]]
-initial_power = 1
+folder_to_parse = '/path/to/parse'  # Замініть на шлях, де ви хочете аналізувати теку
 
-result_energy = game(example_list, initial_power)
-print(f"Загальна енергія гравця: {result_energy}")
+result_files, result_folders = parse_folder(folder_to_parse)
+
+if result_files:
+    print("Знайдені файли:")
+    for file_path in result_files:
+        print(file_path)
+
+if result_folders:
+    print("Знайдені теки:")
+    for folder_path in result_folders:
+        print(folder_path)
+
+if not result_files and not result_folders:
+    print(f"В {folder_to_parse} не знайдено файли або теки.")
