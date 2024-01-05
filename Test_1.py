f26ba501c72821b702c80853c18787376468f9e5
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-05 18:26:29 +0100

    5.1

--- Test_1.py
+++ Test_1.py
@@ -1,10 +1,14 @@
-import sys
-
-def parse_args():
-    # Починаємо з індексу 1, щоб пропустити ім'я скрипта (перший аргумент)
-    result = ' '.join(sys.argv[1:])
-    return result
+def real_len(input_str):
+    control_chars = ['\n', '\f', '\r', '\t', '\v']
+    result_str = ''.join(char for char in input_str if char not in control_chars)
+    return len(result_str)
 
 # Приклад виклику
-args_string = parse_args()
-print(args_string)
+string1 = 'Alex\nKdfe23\t\f\v.\r'
+string2 = 'Al\nKdfe23\t\v.\r'
+
+length1 = real_len(string1)
+length2 = real_len(string2)
+
+print(f'Length of string1 without control characters: {length1}')
+print(f'Length of string2 without control characters: {length2}')
