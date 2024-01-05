4d9887d84b1ef5e36c31014577ad9ce311697751
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-05 15:59:26 +0100

    4.4

--- Test_1.py
+++ Test_1.py
@@ -1,14 +1,35 @@
-def format_ingredients(ingredients_list):
-    if not ingredients_list:
-        return ""
+def get_grade(ects_key):
+    ects_grades = {
+        "F": 1,
+        "FX": 2,
+        "E": 3,
+        "D": 3,
+        "C": 4,
+        "B": 5,
+        "A": 5
+    }
 
-    if len(ingredients_list) == 1:
-        return ingredients_list[0]
+    return ects_grades.get(ects_key, None)
 
-    formatted_ingredients = ", ".join(ingredients_list[:-1]) + " and " + ingredients_list[-1]
-    return formatted_ingredients
+def get_description(ects_key):
+    ects_descriptions = {
+        "F": "Unsatisfactorily",
+        "FX": "Unsatisfactorily",
+        "E": "Enough",
+        "D": "Satisfactorily",
+        "C": "Good",
+        "B": "Very good",
+        "A": "Perfectly"
+    }
+
+    return ects_descriptions.get(ects_key, None)
 
 # Приклад використання:
-ingredients = ["2 eggs", "1 liter sugar", "1 tsp salt", "vinegar"]
-formatted_result = format_ingredients(ingredients)
-print("Форматовані інгредієнти:", formatted_result)
+ects_grade = "D"
+grade_value = get_grade(ects_grade)
+grade_description = get_description(ects_grade)
+
+if grade_value is not None and grade_description is not None:
+    print(f"Оцінка: {grade_value} ({ects_grade}), Пояснення: {grade_description}")
+else:
+    print("Неправильний ключ ECTS.")
