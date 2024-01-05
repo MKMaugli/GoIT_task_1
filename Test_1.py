60881fb0ef85f02e4d95af1c5b40574824ec0128
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-05 20:43:52 +0100

    5.11

--- Test_1.py
+++ Test_1.py
@@ -1,26 +1,11 @@
 import re
 
-def find_word(text, word):
-    result = {'result': False, 'first_index': None, 'last_index': None, 'search_string': word, 'string': text}
-    
-    match = re.search(re.escape(word), text)
+def find_all_words(text, word):
+    pattern = re.compile(r'\b{}\b'.format(re.escape(word)), flags=re.IGNORECASE)
+    matches = pattern.findall(text)
 
-    if match:
-        result['result'] = True
-        result['first_index'] = match.start()
-        result['last_index'] = match.end()
-        result['search_string'] = text[result['first_index']:result['last_index']]
+    return matches if matches else [word]
 
-    return result
-
-# Пример использования
-result1 = find_word(
-    "Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.",
-    "Python")
-
-result2 = find_word(
-    "Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.",
-    "python")
-
-print(result1)
-print(result2)
+# Приклад використання
+result = find_all_words('Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming PYTHOn language, and first released pYthoN it in 1991 as Python 0.9.0. pythOn ', 'Python')
+print(result)
