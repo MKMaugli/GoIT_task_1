6e89dba2b8faff14369bb7cc376b48b8b80f93cf
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-05 22:31:53 +0100

    6.8

--- Test_1.py
+++ Test_1.py
@@ -1,19 +1,41 @@
-def sanitize_file(source, output):
+def save_applicant_data(source, output):
     try:
-        # Відкриття файлу source за допомогою менеджера контексту with у режимі "r"
-        with open(source, 'r') as source_file:
-            # Читання вмісту файла та очищення від цифр
-            source_content = ''.join(char for char in source_file.read() if not char.isdigit())
-
         # Відкриття файлу output за допомогою менеджера контексту with у режимі "w"
         with open(output, 'w') as output_file:
-            # Запис очищеного вмісту файла у файл output
-            output_file.write(source_content)
+            # Ітерація по кожному абітурієнту у списку source
+            for applicant in source:
+                # Запис прізвища, коду спеціальності та балів у файл через кому
+                output_line = f"{applicant['name']},{applicant['specialty']},{applicant['math']},{applicant['lang']},{applicant['eng']}\n"
+                # Запис у файл
+                output_file.write(output_line)
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
 # Приклад використання
-source_file_path = "шлях_до_вихідного_файлу.txt"
-output_file_path = "шлях_до_очищеного_файлу.txt"
-sanitize_file(source_file_path, output_file_path)
+applicants_data = [
+    {
+        "name": "Kovalchuk Oleksiy",
+        "specialty": 301,
+        "math": 175,
+        "lang": 180,
+        "eng": 155,
+    },
+    {
+        "name": "Ivanchuk Boryslav",
+        "specialty": 101,
+        "math": 135,
+        "lang": 150,
+        "eng": 165,
+    },
+    {
+        "name": "Karpenko Dmitro",
+        "specialty": 201,
+        "math": 155,
+        "lang": 175,
+        "eng": 185,
+    },
+]
+
+output_file_path = "шлях_до_вихідного_файлу.txt"
+save_applicant_data(applicants_data, output_file_path)
