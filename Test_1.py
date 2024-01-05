90ffdbca6c1303b0dafbe62c2538891891bba408
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-05 22:41:25 +0100

    6.14

--- Test_1.py
+++ Test_1.py
@@ -1,29 +1,15 @@
 import shutil
 
-def create_backup(path, file_name, employee_residence):
+def unpack(archive_path, path_to_unpack):
     try:
-        # Збереження даних словника у бінарний файл
-        with open(f"{path}/{file_name}", 'wb') as file:
-            for name, country in employee_residence.items():
-                # Запис пари ім'я-країна у бінарний файл
-                line = f"{name} {country}\n"
-                file.write(line.encode('utf-8'))
-
-        # Створення архіву
-        shutil.make_archive(f"backup_{path}", 'zip', path)
-
-        # Повернення шляху до створеного архіву
-        return f"{path}/backup_folder.zip"
+        # Розпакування архіву за допомогою shutil.unpack_archive
+        shutil.unpack_archive(archive_path, path_to_unpack)
+        print(f"Archive successfully unpacked to: {path_to_unpack}")
 
     except Exception as e:
         print(f"An error occurred: {e}")
-        return None
 
 # Приклад використання
-employee_residence_data = {'Michael': 'Canada', 'John': 'USA', 'Liza': 'Australia'}
-backup_path = "шлях_до_теки"
-backup_file_name = "backup_data.bin"
-result = create_backup(backup_path, backup_file_name, employee_residence_data)
-
-if result:
-    print(f"Backup created successfully at: {result}")
+archive_path = "шлях_до_архіву/backup_folder.zip"
+path_to_unpack = "шлях_до_розпакованої_теки"
+unpack(archive_path, path_to_unpack)
