0b9152e102a12349f3facf9be66092a7d56b43ab
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-05 22:30:24 +0100

    6.6

--- Test_1.py
+++ Test_1.py
@@ -1,6 +1,4 @@
-def get_cats_info(path):
-    cats_list = []
-
+def get_recipe(path, search_id):
     # Відкриття файлу за вказаним шляхом у режимі 'r' за допомогою менеджера контексту with
     with open(path, 'r') as file:
         # Читання кожного рядка з файлу
@@ -8,14 +6,17 @@ def get_cats_info(path):
             # Розбиття рядка на складові частини за допомогою коми
             parts = line.strip().split(',')
 
-            # Створення словника із складових частин та додавання його до списку
-            cat_info = {"id": parts[0], "name": parts[1], "age": parts[2]}
-            cats_list.append(cat_info)
+            # Перевірка, чи search_id співпадає з першою частиною рядка (id)
+            if parts[0] == search_id:
+                # Створення словника із складових частин та повернення його
+                recipe = {"id": parts[0], "name": parts[1], "ingredients": parts[2:]}
+                return recipe
 
-    # Повернення списку котів
-    return cats_list
+    # Якщо рецепта із зазначеним search_id у файлі немає, повернення None
+    return None
 
 # Приклад використання
 file_path = "шлях_до_файлу.txt"
-cats_info = get_cats_info(file_path)
-print(cats_info)
+search_id = "60b90c3b13067a15887e1ae4"
+recipe_info = get_recipe(file_path, search_id)
+print(recipe_info)
