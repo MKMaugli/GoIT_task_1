e2041b38b61f0840f5c9c2e97622a2186d254e83
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-05 18:53:42 +0100

    5.4

--- Test_1.py
+++ Test_1.py
@@ -1,22 +1,2 @@
-def sanitize_phone_number(phone):
-    # Удаление всех символов, кроме цифр
-    digits_only = ''.join(char for char in phone if char.isdigit())
-
-    # Проверка и преобразование в правильный формат
-    if len(digits_only) >= 10:  # если номер содержит 10 или более цифр
-        return digits_only
-    else:
-        return None  # возвращаем None для обозначения некорректного номера
-
-# Пример использования
-phone_numbers = [
-    "+38(050)123-32-34",
-    "0503451234",
-]
-
-for phone in phone_numbers:
-    sanitized_number = sanitize_phone_number(phone.strip())  # теперь используем strip()
-    if sanitized_number:
-        print(sanitized_number)
-    else:
-        print(f"Некорректный номер: {phone}")
+def is_check_name(fullname, first_name):
+    return fullname.startswith(first_name)
