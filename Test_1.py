cc7d3929a6c2bbd89caa425f9b055673f3018a14
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-05 22:31:07 +0100

    6.7

--- Test_1.py
+++ Test_1.py
@@ -1,22 +1,19 @@
-def get_recipe(path, search_id):
-    # Відкриття файлу за вказаним шляхом у режимі 'r' за допомогою менеджера контексту with
-    with open(path, 'r') as file:
-        # Читання кожного рядка з файлу
-        for line in file:
-            # Розбиття рядка на складові частини за допомогою коми
-            parts = line.strip().split(',')
+def sanitize_file(source, output):
+    try:
+        # Відкриття файлу source за допомогою менеджера контексту with у режимі "r"
+        with open(source, 'r') as source_file:
+            # Читання вмісту файла та очищення від цифр
+            source_content = ''.join(char for char in source_file.read() if not char.isdigit())
 
-            # Перевірка, чи search_id співпадає з першою частиною рядка (id)
-            if parts[0] == search_id:
-                # Створення словника із складових частин та повернення його
-                recipe = {"id": parts[0], "name": parts[1], "ingredients": parts[2:]}
-                return recipe
+        # Відкриття файлу output за допомогою менеджера контексту with у режимі "w"
+        with open(output, 'w') as output_file:
+            # Запис очищеного вмісту файла у файл output
+            output_file.write(source_content)
 
-    # Якщо рецепта із зазначеним search_id у файлі немає, повернення None
-    return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 # Приклад використання
-file_path = "шлях_до_файлу.txt"
-search_id = "60b90c3b13067a15887e1ae4"
-recipe_info = get_recipe(file_path, search_id)
-print(recipe_info)
+source_file_path = "шлях_до_вихідного_файлу.txt"
+output_file_path = "шлях_до_очищеного_файлу.txt"
+sanitize_file(source_file_path, output_file_path)
