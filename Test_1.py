f72440157b8626e25dc57714b8d8d8bb8411dbf2
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-05 19:30:13 +0100

    5.6

--- Test_1.py
+++ Test_1.py
@@ -1,35 +1,25 @@
-def sanitize_phone_number(phone):
-    new_phone = (
-        phone.strip()
-        .removeprefix("+")
-        .replace("(", "")
-        .replace(")", "")
-        .replace("-", "")
-        .replace(" ", "")
-    )
-    return new_phone
+def is_spam_words(text, spam_words, space_around=False):
+    # Зводимо весь текст до нижнього регістру
+    normalized_text = text.lower()
 
+    for spam_word in spam_words:
+        # Зводимо слово до нижнього регістру
+        normalized_spam_word = spam_word.lower()
 
-def get_phone_numbers_for_countries(list_phones):
-    sanitized_numbers = [sanitize_phone_number(phone) for phone in list_phones]
-    
-    phone_dict = {"UA": [], "JP": [], "TW": [], "SG": []}
-    
-    for phone in sanitized_numbers:
-        if phone.startswith("380") or phone.startswith("0"):
-            phone_dict["UA"].append(phone)
-        elif phone.startswith("81"):
-            phone_dict["JP"].append(phone)
-        elif phone.startswith("886"):
-            phone_dict["TW"].append(phone)
-        elif phone.startswith("65"):
-            phone_dict["SG"].append(phone)
-    
-    return phone_dict
+        if space_around:
+            # Шукаємо слово, яке повинно бути окремим в тексті
+            if f" {normalized_spam_word} " in f" {normalized_text} ":
+                return True
+        else:
+            # Шукаємо слово взагалі
+            if normalized_spam_word in normalized_text:
+                return True
 
-# Виклик функції з вхідними даними
-#input_numbers = ['', '818765347', '8867658976', '657658976']
-input_numbers = ['0658759411', '380998759405', '818765347', '818765344', '8867658976', '657658976']
-result_dict = get_phone_numbers_for_countries(input_numbers)
-print(result_dict)
+    return False
 
+# Приклади використання
+print(is_spam_words("У діда в руках молоток.", ["лоток"]))  # True
+print(is_spam_words("У діда в руках молоток.", ["лоток"], True))  # False
+
+print(is_spam_words("У кота порожній лоток.", ["лоток"]))  # True
+print(is_spam_words("У кота порожній лоток.", ["лоток"], True))  # True
