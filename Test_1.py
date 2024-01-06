639b94342718a7aa4a256553ac43c524b1e308eb
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-06 23:48:31 +0100

    12.3

--- Test_1.py
+++ Test_1.py
@@ -1,10 +1,30 @@
-import json
+import csv
+
 
 def write_contacts_to_file(filename, contacts):
-    with open(filename, "w") as fw:
-        json.dump({'contacts' : (contacts)}, fw)
+    print(contacts)
+    with open(filename, 'w', newline='') as cw:
+        titles = []
+        for title in contacts[0].keys():
+            titles.append(title)
+        field_title = titles
+        writer = csv.DictWriter(cw, fieldnames = field_title)
+        writer.writeheader()
+        for element in contacts:
+            writer.writerow(element)
+    
 
 def read_contacts_from_file(filename):
-    with open(filename, "r") as fr:
-        res = json.load(fr)
-    return res['contacts']
+    result = []
+    with open(filename, 'r', newline='') as cr:
+        reader = csv.DictReader(cr)
+        for row in reader:
+            dict_item = {}
+            for key, val in row.items():
+                if val == 'True':
+                    val = True
+                elif val == 'False':
+                    val = False
+                dict_item.update({key: val})
+            result.append(dict_item)
+    return result
