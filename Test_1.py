f4c61aca44688678a7090dae9cbcbaee7c7f864a
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-06 23:11:32 +0100

    9.5

--- Test_1.py
+++ Test_1.py
@@ -1,4 +1,22 @@
-def discount_price(discount):
-    def cost(price):
-        return price * (1 - discount)
-    return cost
+def format_phone_number(func):
+    def wrapper (phone):
+        res = func(phone)
+        lenres = len(res)
+        if lenres == 12:
+            return '+' + res
+        elif lenres < 12:
+            return '+38' + res
+    return wrapper
+     
+
+@format_phone_number
+def sanitize_phone_number(phone):
+    new_phone = (
+        phone.strip()
+            .removeprefix("+")
+            .replace("(", "")
+            .replace(")", "")
+            .replace("-", "")
+            .replace(" ", "")
+    )
+    return new_phone
