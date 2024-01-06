2f5f3a64da8427626be05b02fdbe1dc18e589200
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-06 23:25:27 +0100

    10.15

--- Test_1.py
+++ Test_1.py
@@ -6,15 +6,22 @@ class Contacts:
 
     def list_contacts(self):
         return self.contacts
-        
 
     def add_contacts(self, name, phone, email, favorite):
-        contact = {
-            "id": Contacts.current_id,
-            "name": name,
-            "phone": phone,
-            "email": email,
-            "favorite": favorite,
-        }
-        self.contacts.append(contact)
+        self.contacts.append(
+            {
+                "id": Contacts.current_id,
+                "name": name,
+                "phone": phone,
+                "email": email,
+                "favorite": favorite,
+            }
+        )
         Contacts.current_id += 1
+
+    def get_contact_by_id(self, id):
+        res = None
+        for i in filter(lambda x: x['id'] == id, self.list_contacts()):
+            if i:
+                res = i
+        return res
