55660ad7f33f143ccfa9f28c3433c88b13f5c563
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-06 23:10:32 +0100

    9.2

--- Test_1.py
+++ Test_1.py
@@ -1,25 +1,8 @@
-def get_grade(key):
-    grade = {"A": 5, "B": 5, "C": 4, "D": 3, "E": 3, "FX": 2, "F": 1}
-    return grade.get(key, None)
+DEFAULT_DISCOUNT = 0.05
 
 
-def get_description(key):
-    description = {
-        "A": "Perfectly",
-        "B": "Very good",
-        "C": "Good",
-        "D": "Satisfactorily",
-        "E": "Enough",
-        "FX": "Unsatisfactorily",
-        "F": "Unsatisfactorily",
-    }
-    return description.get(key, None)
-
-
-def get_student_grade(option):
-    if option == "grade":
-        return get_grade
-    elif option == "description":
-        return get_description
-    else:
-        return None
+def get_discount_price_customer(price, customer):
+    if "discount" in customer:
+        cust_dis = customer.get("discount")
+        return price * (1-cust_dis)
+    return price * (1-DEFAULT_DISCOUNT)
