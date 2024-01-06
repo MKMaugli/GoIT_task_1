aa0595723e2fbf6d70647cf0f5e7546b88693258
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-06 16:03:07 +0100

    8.2

--- Test_1.py
+++ Test_1.py
@@ -1,17 +1,24 @@
-from datetime import datetime
+from datetime import date
 
-def get_days_from_today(date):
-    # Перетворюємо рядок з датою в об'єкт datetime
-    target_date = datetime.strptime(date, "%Y-%m-%d")
-    
-    # Отримуємо поточну дату
-    current_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-    
-    # Визначаємо різницю у днях і повертаємо її
-    days_difference = (current_date - target_date).days
-    return days_difference
+def get_days_in_month(month, year):
+    # Перевіряємо чи місяць коректний
+    if month < 1 or month > 12:
+        raise ValueError("Некоректний номер місяця. Введіть число від 1 до 12.")
+
+    # Визначаємо кількість днів у місяці
+    if month in {1, 3, 5, 7, 8, 10, 12}:
+        return 31
+    elif month in {4, 6, 9, 11}:
+        return 30
+    else:
+        # Перевіряємо чи рік високосний
+        if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+            return 29  # Високосний рік
+        else:
+            return 28  # Звичайний рік
 
 # Приклад використання:
-current_date = datetime.now().strftime("%Y-%m-%d")
-result = get_days_from_today("1989-08-15")
-print(f"Кількість днів з {current_date} до 1989-08-15: {result}")
+month = 2
+year = 2024
+result = get_days_in_month(month, year)
+print(f"У {month}-му місяці {year} року {result} днів.")
