aa494ec35811c005d328d645d26036693be08bcc
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-06 23:48:10 +0100

    12.2

--- Test_1.py
+++ Test_1.py
@@ -1,13 +1,10 @@
-import pickle
-
+import json
 
 def write_contacts_to_file(filename, contacts):
-    with open(filename, "wb") as fw:
-        pickle.dump(contacts, fw)
-        
-
+    with open(filename, "w") as fw:
+        json.dump({'contacts' : (contacts)}, fw)
 
 def read_contacts_from_file(filename):
-    with open(filename, "rb") as fr:
-        unp = pickle.load(fr)
-    return unp
+    with open(filename, "r") as fr:
+        res = json.load(fr)
+    return res['contacts']
