5a20bce40e84f0521ee6eeb7512e1749284189cf
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-06 23:41:40 +0100

    11.4

--- Test_1.py
+++ Test_1.py
@@ -11,9 +11,8 @@ class Point:
 
     @x.setter
     def x(self, x):
-        if type(x) in (int, float):
-            self.__x = x  
-            
+        if (type(x) == int) or (type(x) == float):
+            self.__x = x
 
     @property
     def y(self):
@@ -21,5 +20,20 @@ class Point:
 
     @y.setter
     def y(self, y):
-        if type(y) in (int, float):
+        if (type(y) == int) or (type(y) == float):
             self.__y = y
+
+
+class Vector:
+    def __init__(self, coordinates: Point):
+        self.coordinates = coordinates
+
+    def __setitem__(self, index, value):
+        if index == 0:
+            self.coordinates.x = value
+        else:
+            self.coordinates.y = value
+
+
+    def __getitem__(self, index):
+        return self.coordinates.x if index == 0 else self.coordinates.y
