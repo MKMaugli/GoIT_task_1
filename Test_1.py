f216af21e73e812a22cedc4aac209423e96329c8
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-06 23:12:32 +0100

    9.7

--- Test_1.py
+++ Test_1.py
@@ -1,19 +1,5 @@
-def generator_numbers(string=""):
-    if string == "":
-        return ""        
-    s_before = ""
-    for s in string[::]:
-        if s.isnumeric():
-            s_before += s
-        else:
-            if s_before != "":
-                yield s_before
-                s_before = ""
-    
-
-def sum_profit(string):
-    num = generator_numbers(string)
-    sum = 0
-    for i in num:
-        sum += int(i)
-    return sum
+def normal_name(list_name):
+    res = []
+    for i in map(lambda v: v.capitalize(), list_name):
+        res.append(i)
+    return res
