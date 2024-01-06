22578753157fe832d11cbff553da1f5edb3bf5cd
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-06 23:37:27 +0100

    10.13

--- Test_1.py
+++ Test_1.py
@@ -6,28 +6,22 @@ class Animal:
     def say(self):
         pass
 
+    def change_weight(self, weight):
+        self.weight = weight
+
 
 class Cat(Animal):
     def say(self):
         return "Meow"
 
 
-class Dog(Animal):
+class CatDog:
+    def __init__(self, nickname, weight):
+        self.nickname = nickname
+        self.weight = weight
+
     def say(self):
         return "Woof"
-
-
-class CatDog(Cat, Dog):
-    def __init__(self, nickname, weight):
-        super().__init__(nickname, weight)
-
-    def info(self):
-        return f"{self.nickname}-{self.weight}"
-        
-
-class DogCat(Dog, Cat):
-    def __init__(self, nickname, weight):
-        super().__init__(nickname, weight)
-
-    def info(self):
-        return f"{self.nickname}-{self.weight}"
+    
+    def change_weight(self, weight):
+        self.weight = weight
