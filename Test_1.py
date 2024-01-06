fd5c0f62c4a8d8206bbe1ae1f1dc44b5578f3515
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-06 16:07:21 +0100

    8.4

--- Test_1.py
+++ Test_1.py
@@ -1,24 +1,16 @@
-from datetime import datetime
+from random import randrange
 
-def get_str_date(date):
-    # Перетворюємо рядок у форматі ISO в об'єкт datetime
-    datetime_obj = datetime.fromisoformat(date.replace('Z', '+00:00'))
+def get_numbers_ticket(min_val, max_val, quantity):
+    # Перевіряємо умови обмежень на параметри функції
+    if min_val < 1 or max_val > 1000 or min_val >= quantity or quantity >= max_val:
+        return []
 
-    # Отримуємо назву дня тижня
-    day_of_week = datetime_obj.strftime('%A')
-
-    # Отримуємо число, місяць та рік
-    day = datetime_obj.day
-    month = datetime_obj.strftime('%B')
-    year = datetime_obj.year
-
-    # Додаємо "0" спереду для чисел від 1 до 9
-    formatted_day = f'{day:02}'
-
-    # Повертаємо перетворену дату у вказаному форматі
-    return f'{day_of_week} {formatted_day} {month} {year}'
+    # Генеруємо та повертаємо випадковий набір чисел без дублікатів
+    return sorted([randrange(min_val, max_val) for _ in range(quantity)])
 
 # Приклад використання:
-input_date = '2021-03-03 17:12:40.478Z'
-result = get_str_date(input_date)
+min_val = 1
+max_val = 49
+quantity = 6
+result = get_numbers_ticket(min_val, max_val, quantity)
 print(result)
