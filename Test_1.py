1a07423edcf3fb368e2e0ba9c7e9b3331dbc5a2e
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-06 23:11:57 +0100

    9.6

--- Test_1.py
+++ Test_1.py
@@ -1,22 +1,19 @@
-def format_phone_number(func):
-    def wrapper (phone):
-        res = func(phone)
-        lenres = len(res)
-        if lenres == 12:
-            return '+' + res
-        elif lenres < 12:
-            return '+38' + res
-    return wrapper
-     
+def generator_numbers(string=""):
+    if string == "":
+        return ""        
+    s_before = ""
+    for s in string[::]:
+        if s.isnumeric():
+            s_before += s
+        else:
+            if s_before != "":
+                yield s_before
+                s_before = ""
+    
 
-@format_phone_number
-def sanitize_phone_number(phone):
-    new_phone = (
-        phone.strip()
-            .removeprefix("+")
-            .replace("(", "")
-            .replace(")", "")
-            .replace("-", "")
-            .replace(" ", "")
-    )
-    return new_phone
+def sum_profit(string):
+    num = generator_numbers(string)
+    sum = 0
+    for i in num:
+        sum += int(i)
+    return sum
