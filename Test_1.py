8cc72cd720844f65ad6ee2db8d0742e93d9c1f56
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-06 23:11:12 +0100

    9.4

--- Test_1.py
+++ Test_1.py
@@ -1,14 +1,4 @@
-def caching_fibonacci():
-    cache = {}
-    def fibonacci(n):
-        if n in cache:
-            return cache[n]
-        if n == 0:
-            return 0
-        elif n == 1 or n == 2:
-            return 1
-        else:
-            last_res = cache.setdefault(n, fibonacci(n-1) + fibonacci(n-2))
-            return last_res
-
-    return fibonacci
+def discount_price(discount):
+    def cost(price):
+        return price * (1 - discount)
+    return cost
