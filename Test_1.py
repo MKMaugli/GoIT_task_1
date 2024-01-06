a68e7061da429fadaaa1e2e1cc18c1dcf75abf07
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-06 23:40:37 +0100

    11.3

--- Test_1.py
+++ Test_1.py
@@ -1,20 +1,25 @@
 class Point:
     def __init__(self, x, y):
-        self.__x = x
-        self.__y = y
+        self.__x = None
+        self.__y = None
+        self.x = x
+        self.y = y
 
     @property
     def x(self):
         return self.__x
-    
+
+    @x.setter
+    def x(self, x):
+        if type(x) in (int, float):
+            self.__x = x  
+            
+
     @property
     def y(self):
         return self.__y
 
-    @x.setter
-    def x(self, value):
-        self.__x = value
-
     @y.setter
-    def y(self, value):
-        self.__y = value
+    def y(self, y):
+        if type(y) in (int, float):
+            self.__y = y
