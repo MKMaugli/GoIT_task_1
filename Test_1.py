7a031a9609e1026c721d4e82a0e0f8e877dd7e43
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-06 23:38:45 +0100

    11.2

--- Test_1.py
+++ Test_1.py
@@ -1,4 +1,20 @@
 class Point:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.__x = x
+        self.__y = y
+
+    @property
+    def x(self):
+        return self.__x
+    
+    @property
+    def y(self):
+        return self.__y
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
