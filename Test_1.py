2810c0fc8128cec0547af319064d801d139296cb
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-06 23:34:29 +0100

    10.6

--- Test_1.py
+++ Test_1.py
@@ -9,8 +9,15 @@ class Animal:
     def change_weight(self, weight):
         self.weight = weight
 
-class Cat(Animal):
-    def say(self):
-        return "Meow"
 
-cat = Cat("Simon", 10)
+class Dog(Animal):
+    def __init__(self, nickname, weight, breed):
+        super().__init__(nickname, weight)
+        self.breed = breed
+
+
+    def say(self):
+        return "Woof"
+
+
+dog = Dog("Barbos", 23, "labrador")
