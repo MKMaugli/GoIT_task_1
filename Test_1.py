57a23c8bd4cc3cfbec16c8ea335f27186504e0ba
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-06 23:20:25 +0100

    10.11

--- Test_1.py
+++ Test_1.py
@@ -1,6 +1,9 @@
-from collections import UserList
+from collections import UserString
 
 
-class AmountPaymentList(UserList):
-    def amount_payment(self):
-        return sum(filter(lambda x: x > 0, self.data))
+class NumberString(UserString):
+    def number_count(self):
+        count = 0
+        for i in filter(lambda x: x.isnumeric(), self.data):
+            count += 1
+        return count
