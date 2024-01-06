bb46896bc347cf4e7dff5ae0eab1fa116afb9adb
Kosolap Dmytro <mityakosolap@gmail.com>
2024-01-06 14:09:49 +0100

    7.2

--- Test_1.py
+++ Test_1.py
@@ -1,7 +1,17 @@
 from setuptools import setup
 
-def do_setup(args_dict):
-    setup(**args_dict)
+def do_setup(args_dict, requires):
+    setup(
+        name=args_dict['name'],
+        version=args_dict['version'],
+        description=args_dict['description'],
+        url=args_dict['url'],
+        author=args_dict['author'],
+        author_email=args_dict['author_email'],
+        license=args_dict['license'],
+        packages=args_dict['packages'],
+        install_requires=requires  # Добавлен новый параметр
+    )
 
 # Пример использования:
 my_args_dict = {
@@ -15,4 +25,6 @@ my_args_dict = {
     "packages": ["useful"],
 }
 
-do_setup(my_args_dict)
+my_requires = ["dependency1", "dependency2"]
+
+do_setup(my_args_dict, my_requires)
